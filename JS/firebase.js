1306a6db9f4a5649d30bcadb2f316f21485f6613
JT <Master120344@yahoo.com>
2025-03-17 13:44:47 -0500

    Update firebase.js

--- JS/firebase.js
+++ JS/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAllvq9kMlkbaJzWJfACCqQXZfEV0am2k8",
   authDomain: "kershawlawfirm-38dba.firebaseapp.com",
   projectId: "kershawlawfirm-38dba",
-  storageBucket: "kershawlawfirm-38dba.firebasestorage.app",
+  storageBucket: "kershawlawfirm-38dba.appspot.com",
   messagingSenderId: "246887407742",
   appId: "1:246887407742:web:ddb30806ddb2d3390b382f"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
